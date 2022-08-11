724c5597a11d1dc40fd7368fd3c5a09cced7da82
mokaserahul <106858800+mokaserahul@users.noreply.github.com>
2022-08-11 14:45:49 +0530

    Array

---
+++ MyFirstProgram/Array_Code/Max_Min_Element.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProgram.Array_Code
+{
+    class Max_Min_Element
+    {
+        public static int Max(int[] a)
+        {
+            int max = a[0];
+            for(int i=0;i<a.Length;i++)
+            {
+                if(max<a[i])
+                {
+                    max = a[i];
+                }
+            }
+            return max;
+        }
+        public static int Min(int[] b)
+        {
+            int min = b[0];
+            for (int i = 0; i < b.Length; i++)
+            {
+                if(min>b[i])
+                {
+                    min = b[i];
+                }
+            }
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            int[] n = new int[5];
+            Console.WriteLine("Enter Array Elements:");
+            for(int i=0; i<n.Length;i++)
+            {
+                n[i] = int.Parse(Console.ReadLine());
+            }
+            int max = Max(n);
+            Console.WriteLine("Max:"+max);
+            int min = Min(n);
+            Console.WriteLine("Min:"+min);
+        }
+
+    }
+}
---
+++ MyFirstProgram/Array_Code/Min_Char.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProgram.Array_Code
+{
+    class Min_Char
+    {
+        public static char Minchar(char[] a)
+        {
+            char min = a[0];
+            foreach(int x in a)
+            {
+                Console.WriteLine((char)(x)+" "+x);
+            }
+            for(int i=0;i<a.Length;i++)
+            {
+                if(min>a[i])
+                {
+                    min = a[i];
+                }
+            }
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Array Elements:");
+            char[] c = new char[5];
+            for(int i=0;i<c.Length;i++)
+            {
+                c[i] = char.Parse(Console.ReadLine());
+            }
+            char min = Minchar(c);
+            Console.WriteLine("Min Char:"+min);
+        }
+    }
+}
