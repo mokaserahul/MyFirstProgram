bdbf03bb088c90d5263534c5ba09c77e61491274
mokaserahul <106858800+mokaserahul@users.noreply.github.com>
2022-08-17 12:30:08 +0530

    pattern array

---
+++ MyFirstProgram/Array_2D/Lower_Triangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProgram.Array_2D
+{
+    class Lower_Triangle
+    {
+        public static void Pattern_Tri(int[,] a)
+        {
+            for(int i=0;i<a.GetLength(0);i++)
+            {
+                for(int j=0;j<a.GetLength(1);j++)
+                {
+                    if(i>=j)
+                    {
+                        a[i, j] = int.Parse(Console.ReadLine());
+                    }
+                    else
+                    {
+                        
+                        a[i, j] = 0;
+                    }
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[,] a = new int[5, 5];
+            Console.WriteLine("Enter the Elements:");
+           
+            Pattern_Tri(a);
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write(a[i, j]+" ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ MyFirstProgram/Array_2D/N_Pattern.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProgram.Array_2D
+{
+    class N_Pattern
+    {
+        public static void Pattern(int[,] a)
+        {
+            Console.WriteLine("........................");
+            for(int i=0;i<a.GetLength(0);i++)
+            {
+                for(int j=0;j<a.GetLength(1);j++)
+                {
+                    if ( j == 0 || i == j ||j==a.GetLength(1)-1)
+                    {
+                        Console.Write(a[i,j]+" ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[,] a = new int[4, 4];
+            Console.WriteLine("Enter the Elements:");
+            for(int i=0;i<a.GetLength(0);i++)
+            {
+                for(int j=0;j<a.GetLength(1);j++)
+                {
+                    a[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            for(int i=0;i<a.GetLength(0);i++)
+            {
+                for(int j=0;j<a.GetLength(1);j++)
+                {
+                    Console.Write(a[i,j]);
+                }
+                Console.WriteLine();
+            }
+            Pattern(a);
+
+        }
+    }
+}
